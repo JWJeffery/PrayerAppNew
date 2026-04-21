e492786893746449661f3fd34c5a4c44d1310385
JWJeffery <josh@jwjeffery.org>
2026-04-21 20:38:31 +0000

    //Updated Sunday Theotokion for Orthros, 2024-06-16

--- js/octoechos/orthros-theotokion-sunday.js
+++ js/octoechos/orthros-theotokion-sunday.js
@@ -14,7 +14,7 @@ window.OCTOECHOS.orthros.theotokion.sunday.tones = {
     2: "All-blessed art thou, O Virgin Theotokos, for by Him Who became incarnate through thee hath hades been made captive, Adam restored, the curse annulled, Eve set free, death slain, and we have been given life. Wherefore, chanting, we cry aloud: Blessed is Christ God Who hath been thus well pleased! Glory to Thee!",
     3: "Awed by the beauty of thy virginity and thine all-radiant purity, Gabriel, marvelling, cried out to thee, O Theotokos: “What praise can I bring which is worthy of thee? What shall I call thee? I am at a loss and filled with awe! Wherefore, as I have been commanded, I cry unto thee: Rejoice, O thou who art full of grace!”",
     4: "The mystery hidden from before the ages and unknown even to the angels, through thee, O Theotokos, hath been revealed to those on earth: God incarnate in unconfused union, Who willingly accepted the Cross for our sake and, thereby raising up the first-formed man, hath saved our souls from death.",
-    5: null,
+    5: "Rejoice, O holy mountain trodden by God! Rejoice, animate bush which was not consumed! Rejoice, bridge to God for the world, who alone bearest mortals to everlasting life! Rejoice, incorrupt Maiden who without knowing man gavest birth unto the Salvation of our souls.",
     6: null,
     7: null,
     8: null
